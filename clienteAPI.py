c642825b001a470ce666aa306e4739d4fb63a261
mazvei <mario.azcvei@hotmail.com>
2020-05-25 20:31:49 +0200

    first

---
+++ clienteAPI.py
@@ -0,0 +1,132 @@
+import json
+import requests
+
+opcion=0
+while opcion!=7:
+    print("Seleccione una opcion")
+    print("Web API Bottle - Cliente")
+    print("1 - Listar habitaciones ")
+    print("2 - Buscar una habitacion por ID ")
+    print("3 - Modificar datos de una habitacion")
+    print("4 - Dar de alta una nueva habitación")
+    print("5 - Consultar habitaciones libres u ocupadas ")
+    print("6 - Eliminar una habitación ")
+    print("7 - Salir")
+    opcion = int(input())
+    if opcion==1: #LISTAR
+        print("Listar habitaciones:")
+        r = requests.get('http://localhost:8080/habitaciones')
+        datos=r.json()
+
+        cont = 1
+        for k in datos['habitaciones']:
+            print("Habitacion",(cont),":")
+            cont+=1
+            for i in k.keys():
+                print(""+str(i)+": "+str(k[i]))
+            print("------------------------------")
+
+    elif opcion==2: #BUSCAR POR ID
+        print("Buscar por ID de habitacion")
+        print("Introduce el ID de habitacion")
+        id=input()
+
+        r = requests.get('http://localhost:8080/buscar_habitacion/'+id)
+        data = r.json()
+        print(data['habitacion'])
+
+    elif opcion==3: #MODIFICAR HABITACION
+        print("Modificar habitacion")
+        print("Eliga el ID de la habitacion a modificar")
+        id=input()
+        r = requests.get('http://localhost:8080/buscar_habitacion/'+id)
+        data = r.json()
+        if data['habitacion']=='Error - ID No encontrado':
+            print(data['habitacion'])
+        else:
+            print(data['habitacion'])
+            equipamiento = []
+            print("Introduce el numero de plazas:")
+            plazas = input()
+            print("Cuantos elementos de equipamiento?")
+            elementos = int(input())
+            for i in range(0, elementos):
+                print("Nombre del elemento: ", (i + 1))
+                ele = input()
+                equipamiento.append(ele)
+            print("¿Ocupada? \n 1- Si \n 2- No")
+            ocupada = input()
+            if ocupada == 1:
+                ocupada = True
+            else:
+                ocupada = False
+            modificada = {
+                'id': data['habitacion']['ID'],'plazas': plazas, 'equipamiento': equipamiento, 'ocupada': ocupada}
+            r = requests.post('http://localhost:8080/mod_habitacion/'+id, json=modificada)
+            respuesta = r.json()
+            cont = 1
+            for k in respuesta['habitaciones']:
+                print("Habitacion", (cont), ":")
+                for i in k.keys():
+                    print("" + str(i) + ": " + str(k[i]))
+                print("------------------------------")
+
+    elif opcion==4: #DAR DE ALTA HABITACION
+        equipamiento=[]
+        print("Dar de alta una habitación")
+        print("Introduce el numero de plazas:")
+        plazas=input()
+        print("¿Ocupada? /n 1- Si /n 2- No")
+        ocupada = input()
+        if ocupada==1:
+            ocupada=True
+        else:
+            ocupada=False
+        print("Cuantos elementos de equipamiento?")
+        elementos=int(input())
+        for i in range (0,elementos):
+            print("Nombre del elemento: ",(i+1))
+            ele=input()
+            equipamiento.append(ele)
+        nueva = {
+                'plazas': plazas,
+                'equipamiento':equipamiento,
+                'ocupada': ocupada}
+        r = requests.post('http://localhost:8080/alta_habitacion', json=nueva)
+        print("Habitación almacenada")
+
+
+    elif opcion==5:   # CONSULTAR LIBRES U OCUPADAS
+
+        print("¿Desea consultar habitaciones libres u ocupadas?")
+        print("[1] - Libres")
+        print("[2] - Ocupadas")
+        opcion = input()
+
+        r = requests.get('http://localhost:8080/hab_disponibles/'+opcion)
+        datos = r.json()
+
+        cont = 1
+        for k in datos['habitaciones']:
+            print("Habitacion", (cont), ":")
+            cont += 1
+            for i in k.keys():
+                print("" + str(i) + ": " + str(k[i]))
+            print("------------------------------")
+
+    elif opcion==6:  # ELIMINAR
+        print("Eliminar habitacion:")
+        print("Introduce el ID a eliminar:")
+        id=input()
+        r = requests.delete('http://localhost:8080/eliminar_habitacion/'+id)
+        datos = r.json()
+        cont = 1
+        for k in datos['habitaciones']:
+            print("Habitacion", (cont), ":")
+            for i in k.keys():
+                print("" + str(i) + ": " + str(k[i]))
+            print("------------------------------")
+
+
+
+
---
+++ servidorAPI.py
@@ -0,0 +1,76 @@
+import json
+from random import random, randrange
+
+import bottle
+from Scripts.bottle import route, template
+from bottle import Bottle, run, get, post, put, request, response
+
+app = Bottle()
+habitaciones = [{'ID': 1, 'Plazas': 3, 'Equipamiento': ['Aire acondicionado', 'Wifi', 'Mini bar'], 'Ocupada': False},
+                {'ID': 2, 'Plazas': 4, 'Equipamiento': ['Wifi', 'Caja fuerte', 'Calefaccion'], 'Ocupada': True},
+                {'ID': 3, 'Plazas': 5, 'Equipamiento': ['Wifi', 'Armario', 'Calefaccion'], 'Ocupada': False}]
+
+
+@app.get('/habitaciones')
+def get():
+    return {'habitaciones': habitaciones}
+
+
+@app.get('/buscar_habitacion/<id:int>')
+def get_by_id(id):
+    hab = [hab for hab in habitaciones if hab['ID'] == id]
+    if len(hab) == 0:
+        return {"habitacion": 'Error - ID No encontrado'}
+    else:
+        return {'habitacion': hab[0]}
+
+
+@app.post('/mod_habitacion/<id:int>')
+def update(id):
+    hab = [hab for hab in habitaciones if hab['ID'] == id]
+    if len(hab) == 0:
+        return {"Error": "ID No encontrado"}
+    else:
+        habitaciones.remove(hab[0])
+        nueva = {'ID': request.json.get('id'), 'Plazas': request.json.get('plazas'),
+                 'Equipamiento': request.json.get('equipamiento'),
+                 'Ocupada': request.json.get('ocupada')}
+        habitaciones.append(nueva)
+        return {'habitaciones': habitaciones}
+
+
+@app.get('/hab_disponibles/<id:int>')
+def get_by_id(id):
+    if id == 1:
+        disponibilidad = False
+    else:
+        disponibilidad = True
+    hab = [hab for hab in habitaciones if hab['Ocupada'] == disponibilidad]
+    return {'habitaciones': hab}
+
+
+@app.post('/alta_habitacion')
+def add():
+    estado = True
+    while estado == True:  # Genera Ids aleatorios y comprueba si existen
+        id = randrange(9999)
+        lista_ids = [hab for hab in habitaciones if hab['ID'] == id]
+        if len(lista_ids) == 0:
+            estado = False
+
+    nueva = {'ID': id, 'Plazas': request.json.get('plazas'), 'Equipamiento': request.json.get('equipamiento'),
+             'Ocupada': request.json.get('ocupada')}
+    habitaciones.append(nueva)
+
+
+@app.delete('/eliminar_habitacion/<id:int>')
+def remove(id):
+    hab = [hab for hab in habitaciones if hab['ID'] == id]
+    if len(hab) == 0:
+        return {"Error": "ID No encontrado"}
+    else:
+        habitaciones.remove(hab[0])
+        return {'habitaciones': habitaciones}
+
+
+run(app, host='localhost', port=8080, debug=True, reloader=True)
